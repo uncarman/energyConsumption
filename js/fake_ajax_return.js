ceaaa811375d725d92872eead05139cc50ff0343
wang <sam.wan@sunallies.com>
2019-05-17 15:04:13 +0800

    优化地图显示

--- js/fake_ajax_return.js
+++ js/fake_ajax_return.js
@@ -5,14 +5,18 @@ var fake_data = {
             {
                 id: 1,
                 img: "../imgs/building.png",
-                name: "嘉兴市融通商务中心4号楼嘉兴市融通商务中心4号楼",
-                location: "嘉兴市某某路XXX号"
+                name: "嘉兴市融通商务中心4号楼",
+                location: "嘉兴市某某路XXX号",
+                longitude: 121.542682,
+                latitude: 31.255188,
             },
             {
                 id: 2,
                 img: "./imgs/building.png",
                 name: "嘉兴市融通商务中心5号楼",
-                location: "嘉兴市某某路XXX号"
+                location: "嘉兴市某某路XXX号",
+                longitude: 121.422682,
+                latitude: 31.295188,
             }
         ]
 	},
--- modules/home.js
+++ modules/home.js
@@ -18,12 +18,13 @@ define(function (require) {
             map_id: "map",
             mapPos: null,
             map_zoom_max_limit: 24,
-            map_zoom_max: 12,
-            map_zoom_min: 6,
+            map_zoom_max: 20,
+            map_zoom_min: 8,
             opt: {
                 state: 1
             }
         }
+        $scope.map = null;
         $scope.datas = { ...settings.default_datas, ...datas };
 
         $scope.reset_datas = function($scope, tp) {
@@ -50,26 +51,57 @@ define(function (require) {
         $scope.get_datas_callback = function(res) {
             $scope.$apply(function(){
                 $scope.datas.dataList = res.data;
+                $scope.drawMapPoints();
             });
+        }
+
+        $scope.drawMapPoints = function() {
+            var marker = null;
+            if($scope.datas.dataList.length > 0) {
+                $scope.datas.dataList.map(function(b) {
+                    marker = new AMap.Marker({
+                        position: [b.longitude, b.latitude],
+                        offset: new AMap.Pixel(0, 0),
+                        label: {content: b.name, offset: new AMap.Pixel(0, 62)},
+                        icon: new AMap.Icon({
+                            size: new AMap.Size(60, 60),
+                            image: b.img,
+                            imageSize: new AMap.Size(60, 60),
+                        }),
+                        extData: b.id
+                    });
+                    marker.setMap($scope.map);
+                    marker.on("click", function(e){
+                        $scope.gotoSummary(e.target.getExtData())
+                    });
+                });
+
+                $scope.map.setFitView();
+            }
+            
         }
 
         $scope.gotoSummary = function(id) {
             $scope.goto("building_summary/"+id);
         }
 
-        $scope.init_map_size = function() {
+        $scope.initMapSize = function() {
             var height = document.documentElement.clientHeight - $(".form-inline").height() - 50;
             $("#"+$scope.datas.map_id).css({"height": height});
         }
         
 
+        function showInfoClick(e) {
+            console.log('[ '+e.lnglat.getLng()+','+e.lnglat.getLat()+']');
+        }
+
         // 执行函数
         $scope.get_datas($scope);
 
         // 初始化地图
-        $scope.init_map_size();
+        $scope.initMapSize();
         global.init_map($scope);
-        console.log($scope.map);
+        $scope.map.on('click', showInfoClick);
 
     }])
 });
